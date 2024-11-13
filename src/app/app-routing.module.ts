c5987bb679cccafed7546e6958cf8e2650b47ea0
nAguayoValdebenito <145801865+nAguayoValdebenito@users.noreply.github.com>
2024-11-13 20:30:16 -0300

    CambiosUtils
    cambiosPrograma2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,64 +9,73 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'asistencia',
-    loadChildren: () => import('./pages/asistencia/asistencia.module').then( m => m.AsistenciaPageModule)
+    loadChildren: () => import('./pages/asistencia/asistencia.module').then(m => m.AsistenciaPageModule)
   },
   {
     path: 'alumnos',
-    loadChildren: () => import('./pages/alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+    loadChildren: () => import('./pages/alumnos/alumnos.module').then(m => m.AlumnosPageModule)
   },
   {
     path: 'alumno-profesor',
-    loadChildren: () => import('./pages/alumno-profesor/alumno-profesor.module').then( m => m.AlumnoProfesorPageModule)
+    loadChildren: () => import('./pages/alumno-profesor/alumno-profesor.module').then(m => m.AlumnoProfesorPageModule)
   },
   {
     path: 'mi-asistencia',
-    loadChildren: () => import('./pages/mi-asistencia/mi-asistencia.module').then( m => m.MiAsistenciaPageModule)
+    loadChildren: () => import('./pages/mi-asistencia/mi-asistencia.module').then(m => m.MiAsistenciaPageModule)
   },
   {
     path: 'scaneo',
-    loadChildren: () => import('./pages/scaneo/scaneo.module').then( m => m.ScaneoPageModule)
+    loadChildren: () => import('./pages/scaneo/scaneo.module').then(m => m.ScaneoPageModule)
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
-  },  {
+    loadChildren: () => import('./pages/perfil/perfil.module').then(m => m.PerfilPageModule)
+  },
+  {
     path: 'registro',
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./pages/registro/registro.module').then(m => m.RegistroPageModule)
   },
   {
     path: 'recuperar',
-    loadChildren: () => import('./pages/recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+    loadChildren: () => import('./pages/recuperar/recuperar.module').then(m => m.RecuperarPageModule)
   },
   {
     path: 'arquitectura',
-    loadChildren: () => import('./pages/arquitectura/arquitectura.module').then( m => m.ArquitecturaPageModule)
+    loadChildren: () => import('./pages/arquitectura/arquitectura.module').then(m => m.ArquitecturaPageModule)
   },
   {
     path: 'calidad',
-    loadChildren: () => import('./pages/calidad/calidad.module').then( m => m.CalidadPageModule)
+    loadChildren: () => import('./pages/calidad/calidad.module').then(m => m.CalidadPageModule)
   },
   {
     path: 'etica',
-    loadChildren: () => import('./pages/etica/etica.module').then( m => m.EticaPageModule)
+    loadChildren: () => import('./pages/etica/etica.module').then(m => m.EticaPageModule)
   },
   {
     path: 'agregar-asignatura',
-    loadChildren: () => import('./pages/agregar-asignatura/agregar-asignatura.module').then( m => m.AgregarAsignaturaPageModule)
+    loadChildren: () => import('./pages/agregar-asignatura/agregar-asignatura.module').then(m => m.AgregarAsignaturaPageModule)
   },
   {
     path: 'profesor',
-    loadChildren: () => import('./pages/profesor/profesor.module').then( m => m.ProfesorPageModule)
+    loadChildren: () => import('./pages/profesor/profesor.module').then(m => m.ProfesorPageModule)
   },
-
+  // Añadir rutas específicas para redirección condicional
+  {
+    path: 'vista-alumnos',
+    loadChildren: () => import('./pages/alumnos/alumnos.module').then(m => m.AlumnosPageModule)
+  },
+  {
+    path: 'vista-profesor',
+    loadChildren: () => import('./pages/profesor/profesor.module').then(m => m.ProfesorPageModule)
+  }
 ];
 
 @NgModule({
